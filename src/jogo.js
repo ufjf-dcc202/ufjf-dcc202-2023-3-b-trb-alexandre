b3a634205352eef1342268f887f48cadde9ba0eb
Alexandre <alexandre.vieira@estudante.ufjf.br>
2023-12-17 01:07:31 -0300

    Criando variáveis para os elementos html

--- src/jogo.js
+++ src/jogo.js
@@ -1,22 +1,113 @@
-const tabela_player = [
+let tabela_player = [
   [0, 0, 0],
   [0, 0, 0],
   [0, 0, 0]
 ]
-const linha_soma_player = [0, 0, 0]
-const dado_player = 0
-const pontuacao_player = 0
-const coluna_player = 1
+let linha_soma_player = [0, 0, 0]
+let dado_player = 0
+let pontuacao_player = 0
+let coluna_player = 1
 
-const tabela_enemy = [
+let tabela_enemy = [
   [0, 0, 0],
   [0, 0, 0],
   [0, 0, 0]
 ]
-const linha_soma_enemy = [0, 0, 0]
-const dado_enemy = 0
-const pontuacao_enemy = 0
+let linha_soma_enemy = [0, 0, 0]
+let dado_enemy = 0
+let pontuacao_enemy = 0
 
-const rola_dado = () => {
+let docColunaPlayer
+const setDocColunaPlayer = (val) => {
+  docColunaPlayer = val
+}
+
+let docPontuacaoPlayer
+const setDocPontuacaoPlayer = (val) => {
+  docPontuacaoPlayer = val
+}
+
+let docDadoPlayer
+const setDocDadoPlayer = (val) => {
+  docDadoPlayer = val
+}
+
+let docTabelaPlayer
+const setDocTabelaPlayer = (val) => {
+  docTabelaPlayer = val
+}
+
+let docSomaPlayer
+const setDocSomaPlayer = (val) => {
+  docSomaPlayer = val
+}
+
+let docCelulasPlayer
+const setDocCelulasPlayer = (val) => {
+  docCelulasPlayer = val
+}
+
+let docPontuacaoEnemy
+const setDocPontuacaoEnemy = (val) => {
+  docPontuacaoEnemy = val
+}
+
+let docDadoEnemy
+const setDocDadoEnemy = (val) => {
+  docDadoEnemy = val
+}
+
+let docTabelaEnemy
+const setDocTabelaEnemy = (val) => {
+  docTabelaEnemy = val
+}
+
+let docSomaEnemy
+const setDocSomaEnemy = (val) => {
+  docSomaEnemy = val
+}
+
+let docCelulasEnemy
+const setDocCelulasEnemy = (val) => {
+  docCelulasEnemy = val
+}
+
+// FIM DAS VARIÁVEIS
+
+
+
+
+const rolarDado = () => {
   return Math.floor(Math.random() * 6) + 1
 }
+
+const atualizaDadoJogador = () => {
+  dado_player = rolarDado()
+  docDadoPlayer.innerHTML = dado_player
+}
+
+const aumentarColuna = () => {
+  if (coluna_player < 3)
+    coluna_player += 1
+}
+
+const diminuirColuna = () => {
+  if (coluna_player > 1)
+    coluna_player -= 1
+}
+
+
+export {
+  setDocCelulasEnemy,
+  setDocCelulasPlayer,
+  setDocColunaPlayer,
+  setDocDadoEnemy,
+  setDocDadoPlayer,
+  setDocPontuacaoEnemy,
+  setDocPontuacaoPlayer,
+  setDocSomaEnemy,
+  setDocSomaPlayer,
+  setDocTabelaEnemy,
+  setDocTabelaPlayer,
+  atualizaDadoJogador
+} 
---
+++ src/main.js
@@ -0,0 +1,42 @@
+import {
+  setDocCelulasEnemy,
+  setDocCelulasPlayer,
+  setDocColunaPlayer,
+  setDocDadoEnemy,
+  setDocDadoPlayer,
+  setDocPontuacaoEnemy,
+  setDocPontuacaoPlayer,
+  setDocSomaEnemy,
+  setDocSomaPlayer,
+  setDocTabelaEnemy,
+  setDocTabelaPlayer,
+  atualizaDadoJogador
+} from "./jogo.js"
+
+const docColunaPlayer = document.querySelector('#coluna_player');
+setDocColunaPlayer(docColunaPlayer)
+
+const docPontuacaoPlayer = document.querySelector('#pontuacao_player');
+setDocPontuacaoPlayer(docPontuacaoPlayer)
+
+const docDadoPlayer = document.querySelector('#dado_player');
+setDocDadoPlayer(docDadoPlayer)
+
+const docTabelaPlayer = document.querySelectorAll('#t_player .numero');
+
+const docSomaPlayer = document.querySelectorAll('#s_player .numero');
+
+const docCelulasPlayer = document.querySelectorAll('#t_player .celula');
+
+
+const docPontuacaoEnemy = document.querySelector('#pontuacao_enemy');
+
+const docDadoEnemy = document.querySelector('#dado_enemy');
+
+const docTabelaEnemy = document.querySelectorAll('#t_enemy .numero');
+
+const docSomaEnemy = document.querySelectorAll('#s_enemy .numero');
+
+const docCelulasEnemy = document.querySelectorAll('#t_enemy .celula');
+
+atualizaDadoJogador()
