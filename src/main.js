36de6c5a6d8e81cb92330148b3d9274110c6e241
Alexandre <alexandre.vieira@estudante.ufjf.br>
2023-12-17 01:14:20 -0300

    Adicionando novos sets

--- src/main.js
+++ src/main.js
@@ -35,20 +35,27 @@ const docDadoPlayer = document.querySelector('#dado_player');
 setDocDadoPlayer(docDadoPlayer)
 
 const docTabelaPlayer = document.querySelectorAll('#t_player .numero');
+setDocTabelaPlayer(docTabelaPlayer)
 
 const docSomaPlayer = document.querySelectorAll('#s_player .numero');
+setDocSomaPlayer(docSomaPlayer)
 
 const docCelulasPlayer = document.querySelectorAll('#t_player .celula');
-
+setDocCelulasPlayer(docCelulasPlayer)
 
 const docPontuacaoEnemy = document.querySelector('#pontuacao_enemy');
+setDocPontuacaoEnemy(docPontuacaoEnemy)
 
 const docDadoEnemy = document.querySelector('#dado_enemy');
+setDocDadoEnemy(docDadoEnemy)
 
 const docTabelaEnemy = document.querySelectorAll('#t_enemy .numero');
+setDocTabelaEnemy(docTabelaEnemy)
 
 const docSomaEnemy = document.querySelectorAll('#s_enemy .numero');
+setDocSomaEnemy(docSomaEnemy)
 
 const docCelulasEnemy = document.querySelectorAll('#t_enemy .celula');
+setDocCelulasEnemy(docCelulasEnemy)
 
 atualizaDadoJogador()
